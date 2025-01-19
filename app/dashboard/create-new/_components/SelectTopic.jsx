0a7bdc5e6dd0fdbf29f2389f6a10f5f564b28219
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-19 22:10:46 +0530

    Select Topic function is added to the dashboard page

--- app/dashboard/create-new/_components/SelectTopic.jsx
+++ app/dashboard/create-new/_components/SelectTopic.jsx
@@ -7,21 +7,35 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
 
-function SelectTopic(){
-  const options = ['Custom Prompt','Random AI Story','Scary Story','Historical Facts','Bed Time Story','Motivational','Fun Facts']
+import { useState } from 'react';
+
+function SelectTopic({onUserSelect}){
+  const options = ['Custom Prompt','Random AI Story','Scary Story','Historical Facts','Bed Time Story','Motivational','Fun Facts'];
+  const [selectOption,setselectOption] = useState();
   return (
     <div>
       <h2 className='font-bold text-xl text-primary'>Content</h2>
-      <p className="text-black-100">What is the topic your video</p>
-      <Select>
+      <p className="text-black-100">What is the topic of your video?</p>
+      <Select onValueChange={(value) => {
+        setselectOption(value)
+        value!='Custom Prompt'&&onUserSelect('topic',value)
+        }}>
         <SelectTrigger className="w-[180px]">
           <SelectValue placeholder="Content Type" />
         </SelectTrigger>
         <SelectContent>
-          {options.map((item,index)=>(<SelectItem value={item}>{item}</SelectItem>))}
+          {options.map((item, index) => (
+            <SelectItem key={index} value={item}>{item}</SelectItem>
+          ))}
         </SelectContent>
       </Select>
+      {selectOption === 'Custom Prompt' && (
+        <Textarea className="mt-3" 
+        onChange={(e)=>onUserSelect('topic',e.target.value)}
+        placeholder="Write prompt on which you want to generate video" />
+      )}
     </div>
   );
 }
