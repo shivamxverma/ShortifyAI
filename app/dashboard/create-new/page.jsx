521bc803d643cf599acd8f9460c06a3a0469339e
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-19 22:13:37 +0530

    completed page code for dashboard page

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+"use client"
+import React,{useState} from 'react';
 import SelectTopic from './_components/SelectTopic';
 
 function CreateNewPage(){
+  const [formData,setFromData] = useState();
+  const onHandleInputChange=(fieldName,fieldValue)=>{
+
+  }
   return (
     <div  className="md:px-20">
       <h2 className="font-bold text-4xl text-primary text-center">Create New</h2>
       <div className="mt-10 shadow-md mt-10 p-10">
-         <SelectTopic/>
+         <SelectTopic onUserSelect={onHandleInputChange}/>
+
+         <SelectStyle onUserSelect={onHandleInputChange}/>
       </div>
     </div>
   );
