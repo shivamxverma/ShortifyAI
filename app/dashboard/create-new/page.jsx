7ec6fab893aa223c21b1fed27689c0aa14b0dd83
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-20 19:22:10 +0530

    complete route page for create-new route

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,11 +1,34 @@
 "use client"
 import React,{useState} from 'react';
 import SelectTopic from './_components/SelectTopic';
+import SelectStyle from './_components/SelectStyle';
+import SelectDuration from './_components/SelectDuration';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import CustomLoading from './_components/CustomLoading';
 
 function CreateNewPage(){
   const [formData,setFromData] = useState();
   const onHandleInputChange=(fieldName,fieldValue)=>{
+     setFromData(prev=>({
+      ...prev,
+      [fieldName]:fieldValue
+     }))
+  }
 
+  const onCreateClickHandler = ()=>{
+    GetVideoScript();
+  }
+
+  const GetVideoScript = async() => {
+    const prompt = 'Write a script to generate '+formData.duration+' video on topic : '+formData.topic+' along with Al image prompt in '+formData.imageStyle+' for each scene and give me result in JSON format with imagePrompt and Content Text as field, No Plain text\n';
+
+    console.log(prompt);
+    const result = await axios.post('/api/get-video-script',{
+      prompt:prompt
+    }).then(resp=>{
+      console.log(resp.data);
+    })
   }
   return (
     <div  className="md:px-20">
@@ -14,6 +37,12 @@ function CreateNewPage(){
          <SelectTopic onUserSelect={onHandleInputChange}/>
 
          <SelectStyle onUserSelect={onHandleInputChange}/>
+
+         <SelectDuration onUserSelect={onHandleInputChange}/>
+
+         <Button className="mt-10 w-full" onClick={onCreateClickHandler}>Create Short Video</Button>
+
+         
       </div>
     </div>
   );
