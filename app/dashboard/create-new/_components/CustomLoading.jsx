0f0ba14424065aef605842806d5a257f9bd6bf96
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-01-21 22:10:09 +0530

    Added custom loading component for generating video

--- app/dashboard/create-new/_components/CustomLoading.jsx
+++ app/dashboard/create-new/_components/CustomLoading.jsx
@@ -3,12 +3,17 @@ import {
   AlertDialog,
   AlertDialogContent,
 } from "@/components/ui/alert-dialog"
+import Image from 'next/image';
 
 
 function CustomLoading({loading}){
   return (
       <AlertDialog open={loading}>
-        <AlertDialogContent>
+        <AlertDialogContent className="bg-white">
+          <div className = "bg-white">
+            <Image src={'/progress.gif'} width={100} height = {100}/>
+            <h2>Generating your Video... Do not Refresh</h2>
+          </div>
         </AlertDialogContent>
       </AlertDialog>
   )
